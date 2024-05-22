5ae88dc3e0f6d9b4fe496d88c2d2e1b60207bfa0
Aadrika-Shukla <aadrikashukla19@gmail.com>
2024-05-22 11:17:26 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 from app.forms import *  #to import all the classes declared in forms.py file in  my application
@@ -10,7 +11,25 @@ now we pass that object to dictionary so that we can send the object from backen
 
 
 
-def studentforms(request):
-    ESFO=Student()                  #object creation of our form class we declared in forms.py file
+'''def studentforms(request):
+    ESFO=Student()                  # empty object creation of our form class we declared in forms.py file
     d={'ESFO':ESFO}                 #sending data from backend to frontend
+    return render(request,'studentforms.html',d)'''
+
+
+
+####### RETRIEVING OF FORM DATA /COLLECTING FORM DATA ##########
+
+
+def studentforms(request):
+    ESFO=Student()                  # empty object creation of our form class we declared in forms.py file
+    d={'ESFO':ESFO}                 #sending data from backend to frontend
+    if request.method=='POST':
+        SDFO=Student(request.POST)   #now collected data will come under the wrapper of formobject  with dictionary request.post having my data where my  wrapper is studentforms--form object
+        if SDFO.is_valid():                #is_valid() is to validate the submitted data if valid it returns true else false
+            print(SDFO.cleaned_data)        #collected data is stored in cleaned_data list where our key will be input fields and values will be submitted value for that respective input field
+            return HttpResponse('Data  Submitted Successfully')
+        else:
+            return HttpResponse('INVALID DATA')
+
     return render(request,'studentforms.html',d)
